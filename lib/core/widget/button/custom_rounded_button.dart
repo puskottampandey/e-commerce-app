028cbe513c096cb7d929af9ad9ff52e5fca74711
Puskottam <puskottam@Puskottams-MacBook-Air.local>
2026-03-13 13:28:40 +0545

    feat: add onboarding screen with progress animation

---
+++ lib/core/widget/button/custom_rounded_button.dart
@@ -0,0 +1,78 @@
+import 'package:e_commerce_app/core/theme/constant_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomRoundedButton extends StatefulWidget {
+  final Widget? prefixIcon;
+  final Widget? sufixIcon;
+  final void Function()? onTap;
+  final String title;
+  const CustomRoundedButton({
+    super.key,
+    this.prefixIcon,
+    this.sufixIcon,
+    required this.onTap,
+    required this.title,
+  });
+
+  @override
+  State<CustomRoundedButton> createState() => _CustomRoundedButtonState();
+}
+
+class _CustomRoundedButtonState extends State<CustomRoundedButton> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SizedBox(
+      height: 60.h,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 12.h),
+        child: Material(
+          elevation: 2,
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.circular(16.r),
+          child: InkWell(
+            onTap: widget.onTap,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 20.h),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.r),
+                border: Border.all(color: theme.primaryColor),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (widget.prefixIcon != null)
+                      Row(
+                        children: [
+                          Icon(Icons.sports_basketball, color: AppColors.white),
+                          SizedBox(width: 2.h),
+                        ],
+                      ),
+                    Text(
+                      widget.title,
+                      style: theme.textTheme.displaySmall?.copyWith(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                      ),
+                    ),
+                    if (widget.sufixIcon != null)
+                      Row(
+                        children: [
+                          Icon(Icons.sports_basketball, color: AppColors.white),
+                          SizedBox(width: 2.h),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
