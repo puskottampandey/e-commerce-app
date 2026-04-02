a17d1b16509172b9dd2ec599ea440e244bbdaa37
Puskottam <puskottam@Puskottams-MacBook-Air.local>
2026-04-02 16:19:10 +0545

    feat: local storage add in the app for the user email and other data

---
+++ lib/core/storage/local_storage.dart
@@ -0,0 +1,38 @@
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveSevice {
+  static final HiveSevice _instance = HiveSevice._internal();
+
+  factory HiveSevice() => _instance;
+
+  HiveSevice._internal();
+
+  static const String _boxname = 'appbox';
+
+  // get box
+  Box get _box => Hive.box(_boxname);
+
+  // set box
+  Future<void> setData(String key, dynamic value) async {
+    await _box.put(key, value);
+  }
+
+  //get data
+  T? getData<T>(String key) {
+    return _box.get(key) as T?;
+  }
+
+  // remove data
+  Future<void> removeData(String key) async {
+    await _box.delete(key);
+  }
+
+  // init method
+
+  static Future<void> initHive() async {
+    var directory = await getApplicationDocumentsDirectory();
+    Hive.init(directory.path);
+    await Hive.openBox(_boxname);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:e_commerce_app/core/app/app_dev.dart';
+import 'package:e_commerce_app/core/storage/local_storage.dart';
 import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/injection_container.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await init();
+  await HiveSevice.initHive();
   runApp(const App());
 }
